055d90565bbe295f39ceebf25695a6620f99bafb
Serchinastico <54cymru@gmail.com>
2019-01-21 15:21:07 +0100

    Fix linting

--- app/src/test/java/com/karumi/kataloginlogoutkotlin/PresenterTest.kt
+++ app/src/test/java/com/karumi/kataloginlogoutkotlin/PresenterTest.kt
@@ -39,51 +39,56 @@ class PresenterTest {
     }
 
     @Test
-    fun `should show an invalid credentials error if the log in process returns InvalidCredentials`() = runBlocking {
-        givenTheLogInProcessReturns(InvalidCredentials.left())
+    fun `should show invalid credentials error if the log in process returns InvalidCredentials`() =
+        runBlocking {
+            givenTheLogInProcessReturns(InvalidCredentials.left())
 
-        presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
+            presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
 
-        verify(view).showError(R.string.log_in_error_message)
-    }
-
-    @Test
-    fun `should show an invalid username error if the log in process returns InvalidUsername`() = runBlocking {
-        givenTheLogInProcessReturns(InvalidUsername.left())
-
-        presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
-
-        verify(view).showError(R.string.invalid_username_error_message)
-    }
+            verify(view).showError(R.string.log_in_error_message)
+        }
 
     @Test
-    fun `should show a could not perform log out error if the log out process fails`() = runBlocking {
-        givenTheLogOutProcessReturns(false)
+    fun `should show an invalid username error if the log in process returns InvalidUsername`() =
+        runBlocking {
+            givenTheLogInProcessReturns(InvalidUsername.left())
 
-        presenter.onLogOutButtonTap().join()
+            presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
 
-        verify(view).showError(R.string.log_out_error_message)
-    }
-
-    @Test
-    fun `should hide the log in form and show the log out form if the log in process finished properly`() = runBlocking {
-        givenTheLogInProcessReturns(ANY_USERNAME.right())
-
-        presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
-
-        verify(view).hideLogInForm()
-        verify(view).showLogOutForm()
-    }
+            verify(view).showError(R.string.invalid_username_error_message)
+        }
 
     @Test
-    fun `should hide the log out form and show the log in form if the log out process finished properly`() = runBlocking {
-        givenTheLogOutProcessReturns(true)
+    fun `should show a could not perform log out error if the log out process fails`() =
+        runBlocking {
+            givenTheLogOutProcessReturns(false)
 
-        presenter.onLogOutButtonTap().join()
+            presenter.onLogOutButtonTap().join()
 
-        verify(view).hideLogOutForm()
-        verify(view).showLogInForm()
-    }
+            verify(view).showError(R.string.log_out_error_message)
+        }
+
+    @Test
+    fun `should hide the login form & show the logout form if the login process finishes`() =
+        runBlocking {
+            givenTheLogInProcessReturns(ANY_USERNAME.right())
+
+            presenter.onLogInButtonTap(ANY_USERNAME, ANY_PASS).join()
+
+            verify(view).hideLogInForm()
+            verify(view).showLogOutForm()
+        }
+
+    @Test
+    fun `should hide the logout form and show the login form if the logout process finishes`() =
+        runBlocking {
+            givenTheLogOutProcessReturns(true)
+
+            presenter.onLogOutButtonTap().join()
+
+            verify(view).hideLogOutForm()
+            verify(view).showLogInForm()
+        }
 
     private fun givenTheLogOutProcessReturns(result: Boolean) {
         runBlocking { whenever(kata.logOut()).thenReturn(result) }
